2f39fb9e03e91928b5ba8f5ed4a4ac07a6f789e5
ryougaogahara <157794652+ryougaogahara@users.noreply.github.com>
2024-11-28 11:10:13 +0900

    Create good.py

---
+++ good.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+ 
+st.title('Hello, Streamlit!')
+ 
+feedback = None
+
+
+col1, col2, _ = st.columns([1,1,8]) # 比率を指定
+# col1, col2 = st.columns(2) # 個数のみ指定
+ 
+if st.button('<img draggable="false" role="img" class="emoji" alt="👍" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f44d.svg">'):
+    feedback = 'positive'
+ 
+if st.button('<img draggable="false" role="img" class="emoji" alt="👎" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f44e.svg">'):
+    feedback = 'negative'
+ 
+if feedback:
+    st.write(f'You selected {feedback} feedback.')
