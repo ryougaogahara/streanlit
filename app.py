8bd34fb40bdd3fa096763c97e1ca121eee5fd427
ryougaogahara <157794652+ryougaogahara@users.noreply.github.com>
2024-11-28 10:44:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
 import streamlit as st
+import numpy as np
+import pandas as pd
 
-st.write("おいこれはstreanlit")
+st.title("Web App")
+st.write("**これがstreamlitアプリ**")
 
-if st.button('Say Hello'):
-  st.button('Hello World!')
+answer = st.button('Say hello')
+
+if answer == True:
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+    
+data = {
+    'lat': np.random.randn(100) / 100 + 35.68,
+    'lon': np.random.randn(100) / 100 + 139.75,
+}
+map_data = pd.DataFrame(data)
+# 地図に散布図を描く
+st.map(map_data)
